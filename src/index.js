28fefdf7143c5fdbe209823467be48abdfa9358f
Rixels <mikey.drozd@gmail.com>
2024-08-22 17:31:09 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let unpack = [];
+   matrix.forEach ((value, index) => {
+ unpack = unpack.concat(...matrix[index])
+})
+function ret(a, b){ return a - b};
+unpack.sort(ret)
+  return unpack;
 }
